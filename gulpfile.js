ac979f6589e98ba2709ac86dbf40bebbd5abeeb4
Krainov Alex <a.krainov99@mail.ru>
2018-02-09 11:09:07 +0300

    full

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp           = require('gulp'),
+		gutil          = require('gulp-util' ),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		del            = require('del'),
+		imagemin       = require('gulp-imagemin'),
+		cache          = require('gulp-cache'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		notify         = require("gulp-notify"),
+		rsync          = require('gulp-rsync'),
+		pug            = require('gulp-pug'),
+		plumber        = require('gulp-plumber'),
+		sprite         = require('gulp-plumber');
+
+
+// Работа с Pug
+gulp.task('pug', function() {
+    return gulp.src('app/pug/pages/*.pug')
+        .pipe(plumber())
+        .pipe(pug({
+            pretty: true
+        }))
+        .on("error", notify.onError(function(error) {
+            return "Message to the notifier: " + error.message;
+        }))
+        .pipe(gulp.dest('app'));
+});
+// Пользовательские скрипты проекта
+
+gulp.task('common-js', function() {
+	return gulp.src([
+		'app/js/common.js',
+		])
+	.pipe(plumber({
+		errorHandler: notify.onError(function(error) {
+            return "Message to the notifier: " + error.message;
+        })
+	}))
+	.pipe(concat('common.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('js', ['common-js'], function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-1.8.0/slick/slick.min.js',
+		'app/libs/jquery-validation-1.17.0/main_file.js',
+		'app/libs/jQueryFormStyler-master/dist/jquery.formstyler.min.js',
+		'app/libs/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js'
+		])
+
+	.pipe(concat('libs.min.js'))
+	// .pipe(uglify()) // Минимизировать весь js (на выбор)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// tunnel: true,
+		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+	});
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/pug/**/*.pug', ['pug']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**/*')
+	.pipe(cache(imagemin())) // Cache Images
+});
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
